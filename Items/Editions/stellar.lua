be558e88ceaaf3ab7625c8613979480e7362d9fd
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-09-15 12:18:55 +1000

    Stellar display works properly now

--- Items/Editions/stellar.lua
+++ Items/Editions/stellar.lua
@@ -16,14 +16,14 @@ local stellar = {
         vol = 1
     },
     order = 2,
-    config = { chips = 5, mult = 1 },
+    config = { chip_mod = 5, mult_mod = 1 },
     loc_vars = function(self, info_queue, card)
-        return {vars = {(card.edition or {}).chips or self.config.chips, (card.edition or {}).mult or self.config.mult}}
+        return {vars = {(card.edition or {}).chip_mod or self.config.chip_mod, (card.edition or {}).mult_mod or self.config.mult_mod}}
     end,
     calculate = function(self, card, context)
 		if context.post_joker or (context.main_scoring and context.cardarea == G.play) then
-            local chip = to_number(G.GAME.hands[context.scoring_name].level) * card.edition.chips
-            local mul = to_number(G.GAME.hands[context.scoring_name].level) * card.edition.mult
+            local chip = to_number(G.GAME.hands[context.scoring_name].level) * card.edition.chip_mod
+            local mul = to_number(G.GAME.hands[context.scoring_name].level) * card.edition.mult_mod
 			return {
 				chips = chip,
 				mult = mul
@@ -37,6 +37,31 @@ local stellar = {
         return G.GAME.jest_fairy_edition_rate * self.weight
     end,
 
-    shader = 'stellar'
+    shader = 'stellar',
 }
+
+if JokerDisplay then
+    local jd_edition_def = JokerDisplay.Edition_Definitions
+
+    jd_edition_def["e_aij_stellar"] = {
+        condition_function = function(card)
+            local edition = card.edition
+            return not card.debuff and edition and card.edition.key and card.edition.key == "e_aij_stellar"
+        end,
+        mod_function = function(card)
+            local edition = card.edition
+            local hand_text, _, _ = JokerDisplay.evaluate_hand()
+
+            local chip = 0
+            local mult = 0
+            if hand_text ~= 'Unknown' and G.GAME.hands[hand_text] then
+                chip = to_number(G.GAME.hands[hand_text].level) * edition.chip_mod
+                mult = to_number(G.GAME.hands[hand_text].level) * edition.mult_mod
+            end
+            
+            return { chips = chip, mult = mult }
+        end
+    }
+end
+
 return {name = "Editions", items = {stellar, stellar_shader}}
